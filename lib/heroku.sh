5443107a95c2995d6623b5543017c5bd8be05220
Alan Vardy <alanvardy@gmail.com>
2018-12-13 09:46:27 -0700

    automated heroku app list

--- lib/heroku.sh
+++ lib/heroku.sh
@@ -2,9 +2,12 @@
 # For downloading Heroku Databases via PG:Backup
 source ../settings.conf
 
-apps=(alanvardy hartl-sample-app)
+list=$(heroku apps)
+heroku_apps=( "${list[@]:1}" )
+
 DATE=`date +%Y-%m-%d`
-for i in ${heroku_apps[@]}; do
+for i in $heroku_apps
+do
    heroku pg:backups:capture --app ${i}
    url=$(heroku pg:backups:url --app ${i})
    wget -O $DATE-${i}-Heroku $url
