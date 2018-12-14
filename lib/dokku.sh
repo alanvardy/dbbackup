6bd9a343a910c35a7780db3d92c13a7a53cb0cbb
Alan Vardy <alanvardy@gmail.com>
2018-12-13 19:06:06 -0700

    Update dokku.sh

--- lib/dokku.sh
+++ lib/dokku.sh
@@ -7,7 +7,7 @@ source ../settings.conf
 for server in $dokku_servers
 do
     ymd=$(date "+%Y-%m-%d")
-    db_list=`ssh -l root 178.128.234.219 dokku postgres:list | sed 1d | awk '{print $1}'`
+    db_list=`ssh -t $server dokku postgres:list | sed 1d | awk '{print $1}'`
 
     for db_name in $db_list
     do
